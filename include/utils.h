27b781afbe159c93c1c014426c6df098c7b06d36
Rabin Gaire <rabingaire20@gmail.com>
2020-07-11 21:15:43 +0545

    Refactor stretchy buffer implementation

--- include/utils.h
+++ include/utils.h
@@ -3,8 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <stddef.h>
 #include <stdarg.h>
 
 #include "error_codes.h"
@@ -12,26 +10,5 @@
 char* append(const char*, const char*);
 char* read_from_file(const char*);
 void fatalf(const int, const char*, ...);
-
-#define MAX(x, y) ((x) >= (y) ? (x) : (y))
-
-typedef struct {
-  size_t len;
-  size_t cap;
-  char buf[0];
-} BufHdr;
-
-// Private API
-#define buf__hdr(b) ((BufHdr *)((char *)b - offsetof(BufHdr, buf)))
-#define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
-#define buf__fit(b, n) (buf__fits(b , n) ? 0 : ((b) = buf__grow((b), buf_len(b) + (n), sizeof(*(b)))))
-
-void *buf__grow(const void*, size_t, size_t);
-
-// Public API
-#define buf_len(b) ((b) ? buf__hdr(b)->len: 0)
-#define buf_cap(b) ((b) ? buf__hdr(b)->cap: 0)
-#define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = x, buf__hdr(b)->len++)
-#define buf_free(b) ((b) ? free(buf__hdr(b)): 0)
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -7,21 +7,6 @@ char* append(const char* dest, const char* source) {
   return result;
 }
 
-void *buf__grow(const void *buf, size_t new_len, size_t elem_size) {
-  size_t new_cap = MAX(1 + 2 * buf_cap(buf), new_len);
-  size_t new_size = offsetof(BufHdr, buf) + new_cap * elem_size;
-  BufHdr *new_hdr;
-  if(buf) {
-    new_hdr = (BufHdr *)realloc(buf__hdr(buf), new_size);
-  } else {
-    new_hdr = (BufHdr *)malloc(new_size);
-    new_hdr->len = 0;
-  }
-  new_hdr->cap = new_cap;
-  return new_hdr->buf;
-}
-
-
 // TODO: rewrite this with better error handling
 char* read_from_file(const char* file_path) {
   FILE* f = fopen(file_path, "r");
